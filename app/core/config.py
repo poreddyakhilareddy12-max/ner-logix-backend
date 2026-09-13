ba0c62b02965687f11cc13eac8a6c27cbd61e6d3
poreddyakhilareddy12-max <poreddyakhilareddy12@gmail.com>
2026-09-13 16:27:18 +0530

    Use environment variable for secret key

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     VERSION: str = '1.0.0'
     API_V1_STR: str = '/api'
     
-    SECRET_KEY: str = 'ner-logix-super-secure-production-secret-key-2026-sih'
+    SECRET_KEY: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
     
