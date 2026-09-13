994ad8441b99a23f7857025bc772ad56f28c4932
poreddyakhilareddy12-max <poreddyakhilareddy12@gmail.com>
2026-09-13 20:31:40 +0530

    Allow deployed frontend origin

--- app/core/config.py
+++ app/core/config.py
@@ -18,8 +18,12 @@ class Settings(BaseSettings):
     
     UPLOAD_DIR: str = os.path.join(BASE_DIR, 'uploads').replace('\\', '/')
     MAX_UPLOAD_SIZE_MB: int = 10
-    
-    CORS_ORIGINS: List[str] = ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000']
+    CORS_ORIGINS: List[str] = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:3000',
+    'https://ner-logix-frontend-6rdr.onrender.com'
+]
     
     class Config:
         case_sensitive = True
